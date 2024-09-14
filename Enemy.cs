fbf17a2ea80b41d6be609166bc41ab2c84915f19
Bunhaur <86873526+Bunhaur@users.noreply.github.com>
2024-09-14 15:08:15 +0400

    Update Enemy.cs

--- Enemy.cs
+++ Enemy.cs
@@ -5,19 +5,8 @@ public class Enemy : MonoBehaviour
     private Player _target;
     private float _speed = 8;
 
-    private void Update()
-    {
-        Move(_target);
-    }
-
     public void SetTarget(Player target)
     {
         _target = target;
-    }
-
-    private void Move(Player player)
-    {
-        transform.position = Vector3.MoveTowards(transform.position, player.transform.position, _speed * Time.deltaTime);
-        transform.LookAt(player.transform);
     }
 }
